e53019587f3c1b525d7ec2a531fee568e227d1b8
Elinor <elinor@unikhorn.io>
2025-03-20 15:52:07 +0100

    Sort only emails

--- emails_sorting.py
+++ emails_sorting.py
@@ -2,8 +2,8 @@ import csv
 
 # Files to compare
 complete_file = "complete.csv"    # Complete list of contacts
-sent_file = "sent.csv"     # List of contacts who received an email
-output_file = "output.csv"     # List of contacts who did not receive an email
+sent_file = "lot1.csv"     # List of contacts who received an email
+output_file = "lot2.csv"     # List of contacts who did not receive an email
 
 # Compare the list of contacts who received an email with the complete list
 with open(sent_file, "r", encoding="utf-8") as f:
@@ -14,10 +14,10 @@ with open(sent_file, "r", encoding="utf-8") as f:
 with open(complete_file, "r", encoding="utf-8") as f, open(output_file, "w", encoding="utf-8", newline="") as out:
     reader = csv.reader(f)
     writer = csv.writer(out)
-    
+
     for row in reader:
         email = row[0].strip()  # We assume that emails are in the first column
         if email not in emails_sent:
-            writer.writerow(row)  # Write the row in the output file
+            writer.writerow([email])  # Write the row in the output file
 
 print(f"✅ Sorting done! The remaining emails are in '{output_file}'")
